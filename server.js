78bffac79de59baa1e8af20f0e9e2b80f29ee295
cissou <abdessatarbourai@gmail.com>
2025-04-06 22:54:30 +0200

    kkkklolefefe*l,ojjiho

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-
-
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-// Servir les fichiers statiques (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname)));
+// Servir les fichiers statiques depuis le dossier 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route principale pour servir la page d'accueil
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'accueil.html'));
+    res.sendFile(path.join(__dirname, 'public', 'accueil.html'));
 });
 
 // Route pour servir la page quiz.html
 app.get('/quiz.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'quiz.html'));
+    res.sendFile(path.join(__dirname, 'public', 'quiz.html'));
 });
 
 // Route pour servir la page note.html
 app.get('/note.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'note.html'));
+    res.sendFile(path.join(__dirname, 'public', 'note.html'));
 });
 
 // Démarrer le serveur
 app.listen(PORT, () => {
-    // Sur Vercel, il n'est pas nécessaire de spécifier une URL complète, le port est géré automatiquement.
     console.log(`🚀 Serveur en écoute sur le port ${PORT}`);
-  });
+});
